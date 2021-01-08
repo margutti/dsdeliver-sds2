7e9c96e554fefc38630b7e01031553636ebbadcc
Sergio Roberto Margutti <margutti@softmidia.com.br>
2021-01-08 12:05:16 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
