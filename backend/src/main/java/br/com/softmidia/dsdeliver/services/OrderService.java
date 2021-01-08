aff6e46940957c765456f16fb3f20d84fb8cae8a
Sergio Roberto Margutti <margutti@softmidia.com.br>
2021-01-07 22:26:15 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/br/com/softmidia/dsdeliver/services/OrderService.java
+++ backend/src/main/java/br/com/softmidia/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package br.com.softmidia.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,45 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.softmidia.dsdeliver.dto.OrderDTO;
+import br.com.softmidia.dsdeliver.dto.ProductDTO;
 import br.com.softmidia.dsdeliver.entities.Order;
+import br.com.softmidia.dsdeliver.entities.OrderStatus;
+import br.com.softmidia.dsdeliver.entities.Product;
 import br.com.softmidia.dsdeliver.repositories.OrderRepository;
+import br.com.softmidia.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
 }
