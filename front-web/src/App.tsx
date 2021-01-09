9588628e36281ebb960099c8bb317a4ed5822f11
Sergio Roberto Margutti <margutti@softmidia.com.br>
2021-01-09 00:19:20 -0300

    Ajustando erros

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,7 +2,6 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Routes from './Routes';
 
